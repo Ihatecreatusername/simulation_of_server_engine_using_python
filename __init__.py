64997c7a3795b00b8dce2474c8a1375895e9da53
ju <6434352+Ihatecreatusername@users.noreply.github.com>
2021-09-18 23:18:28 +0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,39 +1,5 @@
 #-*- coding:utf-8 -*-
-import traceback
-import platform
-
 import log
-import command
 
-def Quit(sCommand):
-    log.log_file("system", "quit")
-    import os
-    import signal
-    os.kill(os.getpid(), signal.SIGINT)
-
-if __name__ == "__main__":
-    log.Init()
-    log.log_file("system", "system start")
-
-    import task
-    
-    task.Init()
-
-    if platform.system() == "Windows":
-        import mywatchdog
-        mywatchdog.Init()
-
-    log.log_file("system", "system init done")
-
-    while True:
-        try:
-            sCommand = input()
-            lstCommand = sCommand.split(" ")
-            oFunc = getattr(command, lstCommand[0])
-            if oFunc:
-                oFunc(sCommand)
-        except:
-            msg = traceback.format_exc()
-            print(msg)
-            log.log_file("err", msg)
-
+def Init():
+    log.log_file("system", "task moudle done")
